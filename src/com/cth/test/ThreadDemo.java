e62411ab4ca2dde932de9e679f4c5c57a043a117
SherlockTH <chenth101002101@gmail.com>
2017-05-09 10:36:04 +0800

    updata

--- src/com/cth/test/ThreadDemo.java
+++ src/com/cth/test/ThreadDemo.java
@@ -1,32 +1,32 @@
 package com.cth.test;
 
-//class MyThread1 extends Thread{  
-//      
-//	private int  ticket = 10;  
-//    private String name;  
-//    public MyThread1(String name){  
-//        this.name =name;  
-//    }  
-//      
-//    public void run(){  
-//    	
-//        for(int i =0;i<500;i++){  
-//            if(this.ticket>0){  
-//                System.out.println(this.name+"卖票---->"+(this.ticket--));  
-//            }  
-//        }  
-//    }  
-//}  
-public class ThreadDemo {  
-  
-      
-    public static void main(String[] args) {  
-//        MyThread1 mt1= new MyThread1("一号窗口");  
-//        MyThread1 mt2= new MyThread1("二号窗口");  
-//        MyThread1 mt3= new MyThread1("三号窗口");  
-//        mt1.start();  
-//        mt2.start();  
-//        mt3.start();  
-    }  
-  
-}  
+class MyThread2 extends Thread {
+
+	private int ticket = 10;
+	private String name;
+
+	public MyThread2(String name) {
+		this.name = name;
+	}
+
+	public void run() {
+		for (int i = 0; i < 500; i++) {
+			if (this.ticket > 0) {
+				System.out.println(this.name + "鍗栫エ---->" + (this.ticket--));
+			}
+		}
+	}
+}
+
+public class ThreadDemo {
+
+	public static void main(String[] args) {
+		MyThread2 mt1 = new MyThread2("涓�鍙风獥鍙�");
+		MyThread2 mt2 = new MyThread2("浜屽彿绐楀彛");
+		MyThread2 mt3 = new MyThread2("涓夊彿绐楀彛");
+		mt1.start();
+		mt2.start();
+		mt3.start();
+	}
+
+}
