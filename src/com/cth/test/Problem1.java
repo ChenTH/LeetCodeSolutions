99f8c3c49caa5b94667d41ae0e5087b4fb669be6
ChenTH <chenth101002101@gmail.com>
2017-05-11 11:26:42 +0800

    updata

--- src/com/cth/test/Problem1.java
+++ src/com/cth/test/Problem1.java
@@ -1,8 +1,20 @@
 package com.cth.test;
 
-import com.cth.decode.tree.TreeNode;;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class Problem1 {
 	public static void main(String[] args){
-		
+		HashMap<Integer, Integer> map=new HashMap<>();
+		map.put(1, 1);
+		Hashtable<Integer, Integer> table=new Hashtable<>();
+		table.put(1, 1);
+		Set<Integer> set=new HashSet<>();
+		set.add(1);
+		ConcurrentHashMap<Integer, Integer> cMap=new ConcurrentHashMap<>();
+		cMap.put(1,1);
 	}
 }
