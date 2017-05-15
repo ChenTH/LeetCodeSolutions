371af513d713735fa2c0fff0aede9d90580de9dd
ChenTH <chenth101002101@gmail.com>
2017-05-15 10:10:04 +0800

    update

---
+++ src/com/cth/decode/array/KdiffPairsinanArray.java
@@ -0,0 +1,35 @@
+package com.cth.decode.array;
+
+import java.util.*;
+
+/**
+ * Created by SherlockTHao on 2017/5/15.
+ */
+public class KdiffPairsinanArray {
+    public static void main(String[] args){
+        int[] nums={3,1,4,1,5};
+        findPairs(nums,2);
+    }
+    public static int findPairs(int[] nums, int k) {
+        if(nums ==null || k<0){ return  0;}
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i :nums){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        int result=0;
+        for(Integer num : map.keySet()){
+            if(k==0){
+                if(map.get(num)>=2){
+                    result++;
+                }
+            }
+            else {
+                if(map.containsKey(num+k)){
+                    result++;
+                }
+            }
+        }
+        return  result;
+    }
+
+}
