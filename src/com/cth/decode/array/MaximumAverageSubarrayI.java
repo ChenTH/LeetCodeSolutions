a9f434288e3e626af612fc88d808c3c6d68fbeaf
ChenTH <chenth101002101@gmail.com>
2017-07-27 16:07:10 +0800

    update

---
+++ src/com/cth/decode/array/MaximumAverageSubarrayI.java
@@ -0,0 +1,27 @@
+package com.cth.decode.array;
+
+/**
+ * Created by SherlockTHao on 2017/7/27.
+ */
+public class MaximumAverageSubarrayI {
+    public static void main(String[] args){
+        System.out.print(findMaxAverage(new int[]{0,4,0,3,2},1));
+    }
+    public static double findMaxAverage(int[] nums, int k) {
+        if(nums.length<k){
+            return 0;
+        }
+        int sum=0;
+        for(int i=0;i<k;i++){
+            sum+=nums[i];
+        }
+        int max=sum;
+        for(int i=k;i<nums.length;i++){
+            sum=sum-nums[i-k]+nums[i];
+            if(max<sum){
+                max=sum;
+            }
+        }
+        return (double) max/k;
+    }
+}
