8e0cd6fd751c2a79ed70b48daa48530fbe208f13
sherlockthao <chenth101002101@gmail.com>
2018-04-04 16:13:19 +0800

    增加了一些题目的解答

---
+++ src/com/cth/decode/hashtable/Easy/JewelsandStones.java
@@ -0,0 +1,22 @@
+package com.cth.decode.hashtable.Easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class JewelsandStones {
+    public int numJewelsInStones(String J, String S) {
+        char[] jchars=J.toCharArray();
+        char[] schars=S.toCharArray();
+        Set<Character> set=new HashSet<Character>();
+        int count=0;
+        for(char c:jchars){
+            set.add(c);
+        }
+        for(char c:schars){
+            if(set.contains(c)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
---
+++ src/com/cth/decode/hashtable/Easy/UniqueMorseCodeWords.java
@@ -0,0 +1,26 @@
+package com.cth.decode.hashtable.Easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueMorseCodeWords {
+    public static void main(String[] args){
+        String[] strings={"rwjje","aittjje","auyyn","lqtktn","lmjwn"};
+       System.out.println(uniqueMorseRepresentations(strings));
+    }
+    public static int uniqueMorseRepresentations(String[] words) {
+        String fullTable=".-,-...,-.-.,-..,.,..-.,--.,....,..,.---,-.-,.-..,--,-.,---,.--.,--.-,.-.,...,-,..-,...-,.--,-..-,-.--,--..";
+        String[] codes=fullTable.split(",");
+        Set<String> set=new HashSet<>();
+        for(String s:words){
+            StringBuilder sb=new StringBuilder();
+            char[] chars=s.toCharArray();
+            for(char c:chars){
+                sb.append(codes[c-'a']);
+            }
+            System.out.println(sb.toString());
+            set.add(sb.toString());
+        }
+        return set.size();
+    }
+}
