52652be7b0d159cca96b4dda35625f7718658e3f
sherlockthao <chenth101002101@gmail.com>
2018-04-10 10:15:22 +0800

    增加了一些题目的解答

---
+++ src/com/cth/decode/stack/Easy/BaseballGame.java
@@ -0,0 +1,26 @@
+package com.cth.decode.stack.Easy;
+
+import java.util.Stack;
+
+public class BaseballGame {
+    public static void main(String[] args){
+        String[] ops={"5","2","C","D","+"};
+        calPoints(ops);
+    }
+    public static int calPoints(String[] ops) {
+        Stack<Integer> stack=new Stack<>();
+        for(String s:ops){
+            switch (s){
+                case "D" : stack.push(stack.peek()*2);break;
+                case "C" : stack.pop(); break;
+                case "+" : stack.push(stack.get(stack.size()-1)+stack.get(stack.size()-2));break;
+                default:stack.push(Integer.parseInt(s));
+            }
+        }
+        int result=0;
+        while (!stack.empty()){
+            result+=stack.pop();
+        }
+        return result;
+    }
+}
